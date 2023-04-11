80833a20506cfeccbfa6fa348d5b34ee81fac726
Rhilda <ildariismagilov@mail.ru>
2023-04-11 21:07:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+import scipy.stats as stats
 
 chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    alpha = 0.09
-    pvalue = ztest(x, value=300, alternative='smaller')[1]
-    return pvalue < alpha 
+    return stats.ttest_ind(x, y)[1] < 0.09
